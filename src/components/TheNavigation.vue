963dfb3ced0b0bf586ea78af49ce01db7370f89b
ntbinh6 <ntbinh6@cmc.com.vn>
2021-03-13 16:29:41 +0700

    vue-router-app

---
+++ src/components/TheNavigation.vue
@@ -0,0 +1,29 @@
+<template>
+  <nav id="nav">
+    <router-link to="/">Home</router-link>
+    <router-link to="/about">About</router-link>
+    <router-link to="/brazil">Home</router-link>
+    <router-link to="/panama">Panama</router-link>
+    <router-link to="/hawaii">Hawaii</router-link>
+    <router-link to="/jamaica">Jamaica</router-link>
+  </nav>
+</template>
+<style scoped>
+#nav {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+  padding: 0 10px;
+  text-decoration: none;
+}
+
+#nav a.router-link-exact-active {
+  color: green;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -8,7 +8,7 @@ const routes = [
   {
     path: "/",
     name: "Home",
-    component: Home
+    component: Home,
   },
   {
     path: "/about",
@@ -17,14 +17,40 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
-  }
+      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+    // ở trên có đoạn webpackChunkName là 1 magic comment, khi split 1 file js lớn ra thành các file js con như này thì việc
+    // đặt tên cho các file js con là cần thiết để có thể debug hiệu quả, và muốn đặt tên như nào thì dùng magic cmt phía trên
+  },
+  {
+    path: "/brazil",
+    name: "Brazil",
+    component: () =>
+      import(/* webpackChunkName: "brazil" */ "../views/Brazil.vue"),
+  },
+  {
+    path: "/panama",
+    name: "Panama",
+    component: () =>
+      import(/* webpackChunkName: "panama" */ "../views/Panama.vue"),
+  },
+  {
+    path: "/hawaii",
+    name: "Hawaii",
+    component: () =>
+      import(/* webpackChunkName: "hawaii" */ "../views/Hawaii.vue"),
+  },
+  {
+    path: "/jamaica",
+    name: "Jamaica",
+    component: () =>
+      import(/* webpackChunkName: "jamaica" */ "../views/Jamaica.vue"),
+  },
 ];
 
 const router = new VueRouter({
   mode: "history",
   base: process.env.BASE_URL,
-  routes
+  routes,
 });
 
 export default router;
